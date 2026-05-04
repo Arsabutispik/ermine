ca1601c3c47d413dda2b3de9c978dd7479753930
Arsabutispik <ispik@ispik.dev>
2026-05-04 23:23:05 +0300

    refactor: code formating

--- Ermine/Views/MainChatView.axaml.cs
+++ Ermine/Views/MainChatView.axaml.cs
@@ -1,52 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
-using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using AsyncImageLoader;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Input.Platform;
 using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
 using Avalonia.Platform.Storage;
 using Avalonia.Threading;
 using Avalonia.VisualTree;
 using CommunityToolkit.Mvvm.Messaging;
+using Ermine.Models;
 using Ermine.ViewModels;
 using Serilog;
-using Avalonia.Input;
-using Avalonia.Input.Platform;
-using Avalonia.Media.Imaging;
-using Ermine.Models;
 
 namespace Ermine.Views;
 
 public partial class MainChatView : UserControl
 {
-    private MainChatViewModel? _boundViewModel;
     private ObservableCollection<Message>? _boundMessages;
+    private MainChatViewModel? _boundViewModel;
     private ScrollViewer? _messageScrollViewer;
+    private CancellationTokenSource? _scrollCts;
     private bool _stickToBottom = true;
+
     public MainChatView()
     {
-        InitializeComponent();  
+        InitializeComponent();
         AddHandler(DragDrop.DragOverEvent, OnDragOver);
         AddHandler(DragDrop.DropEvent, OnDrop);
         var textBox = this.FindControl<TextBox>("InputTextBox");
         textBox?.AddHandler(KeyDownEvent, OnTextBoxKeyDown, RoutingStrategies.Tunnel);
-        WeakReferenceMessenger.Default.Register<PickFilesMessage>(this, (_, msg) =>
-        {
-            msg.Reply(PickFilesAsync(msg.Options));
-        });
-        
+        WeakReferenceMessenger.Default.Register<PickFilesMessage>(this,
+            (_, msg) => { msg.Reply(PickFilesAsync(msg.Options)); });
+
         WeakReferenceMessenger.Default.Register<MainChatViewModel.ScrollToMessageRequest>(this, (_, msg) =>
         {
             Dispatcher.UIThread.InvokeAsync(() =>
             {
                 MessageList.ScrollIntoView(msg.Target);
-        
-                _stickToBottom = false; 
+
+                _stickToBottom = false;
             });
         });
         MessageList.ContainerClearing += (s, e) =>
@@ -54,11 +55,12 @@ public partial class MainChatView : UserControl
             var imageControls = e.Container.GetVisualDescendants().OfType<Image>();
             foreach (var imageControl in imageControls)
             {
-                AsyncImageLoader.ImageLoader.SetSource(imageControl, null);
-                imageControl.Source = null; 
+                ImageLoader.SetSource(imageControl, null);
+                imageControl.Source = null;
             }
         };
     }
+
     protected override void OnLoaded(RoutedEventArgs e)
     {
         base.OnLoaded(e);
@@ -78,7 +80,6 @@ public partial class MainChatView : UserControl
         base.OnDataContextChanged(e);
         AttachToViewModel();
     }
-    private CancellationTokenSource? _scrollCts;
 
     private void AttachToViewModel()
     {
@@ -136,7 +137,7 @@ public partial class MainChatView : UserControl
 
         AttachToMessages(_boundViewModel.CurrentMessages);
         _stickToBottom = true;
-        ScrollToBottomAfterLayout(force: true);
+        ScrollToBottomAfterLayout(true);
     }
 
     private void OnMessagesChanged(object? sender, NotifyCollectionChangedEventArgs e)
@@ -144,7 +145,7 @@ public partial class MainChatView : UserControl
         if (e.Action != NotifyCollectionChangedAction.Add) return;
 
         _stickToBottom = true;
-        ScrollToBottomAfterLayout(force: false);
+        ScrollToBottomAfterLayout(false);
     }
 
     private void AttachToMessageScrollViewer()
@@ -174,7 +175,8 @@ public partial class MainChatView : UserControl
         if (_messageScrollViewer == null)
             return;
 
-        var isAtBottom = _messageScrollViewer.Offset.Y >= (_messageScrollViewer.Extent.Height - _messageScrollViewer.Viewport.Height - 20);
+        var isAtBottom = _messageScrollViewer.Offset.Y >=
+                         _messageScrollViewer.Extent.Height - _messageScrollViewer.Viewport.Height - 20;
 
         if (isAtBottom)
         {
@@ -193,7 +195,7 @@ public partial class MainChatView : UserControl
 
         if (!force && !_stickToBottom)
         {
-            var isAtBottom = scrollViewer.Offset.Y >= (scrollViewer.Extent.Height - scrollViewer.Viewport.Height - 50);
+            var isAtBottom = scrollViewer.Offset.Y >= scrollViewer.Extent.Height - scrollViewer.Viewport.Height - 50;
             if (!isAtBottom && scrollViewer.Extent.Height != 0) return;
         }
 
@@ -219,16 +221,14 @@ public partial class MainChatView : UserControl
             Log.Error(ex, "Error while scrolling message list to bottom");
         }
     }
+
     private async Task<IReadOnlyList<IStorageFile>?> PickFilesAsync(FilePickerOpenOptions options)
     {
         try
         {
             var window = TopLevel.GetTopLevel(this);
-            if (window?.StorageProvider is { } sp)
-            {
-                return await sp.OpenFilePickerAsync(options);
-            }
-        
+            if (window?.StorageProvider is { } sp) return await sp.OpenFilePickerAsync(options);
+
             return null;
         }
         catch (Exception ex)
@@ -237,6 +237,7 @@ public partial class MainChatView : UserControl
             return null;
         }
     }
+
     private static void OnDragOver(object? sender, DragEventArgs e)
     {
         e.DragEffects = e.DataTransfer.TryGetFiles() is not null ? DragDropEffects.Copy : DragDropEffects.None;
@@ -245,11 +246,9 @@ public partial class MainChatView : UserControl
     private void OnDrop(object? sender, DragEventArgs e)
     {
         var files = e.DataTransfer.TryGetFiles();
-        if (files is not null)
-        {
-            _ = ProcessDroppedOrPastedFilesAsync(files);
-        }
+        if (files is not null) _ = ProcessDroppedOrPastedFilesAsync(files);
     }
+
     private async void OnTextBoxKeyDown(object? sender, KeyEventArgs e)
     {
         try
@@ -265,7 +264,7 @@ public partial class MainChatView : UserControl
                 var files = await clipboard.TryGetFilesAsync();
                 if (files != null && files.Length != 0)
                 {
-                    e.Handled = true; 
+                    e.Handled = true;
                     await ProcessDroppedOrPastedFilesAsync(files);
                 }
 
@@ -282,16 +281,15 @@ public partial class MainChatView : UserControl
             Log.Error(ex, "Error while processing pasted files");
         }
     }
+
     private async Task ProcessDroppedOrPastedFilesAsync(IEnumerable<IStorageItem> items)
     {
         var storageFiles = items.OfType<IStorageFile>().ToList();
         if (storageFiles.Count == 0) return;
-        
-        if (DataContext is MainChatViewModel vm)
-        {
-            await vm.StageFilesAsync(storageFiles);
-        }
+
+        if (DataContext is MainChatViewModel vm) await vm.StageFilesAsync(storageFiles);
     }
+
     private Task ProcessPastedBitmapAsync(Bitmap bitmap)
     {
         try
@@ -300,7 +298,7 @@ public partial class MainChatView : UserControl
             using var ms = new MemoryStream();
             bitmap.Save(ms);
             var bytes = ms.ToArray();
-            
+
             var attachment = new StagedAttachment
             {
                 FileName = $"Pasted_Image_{DateTime.Now:yyyyMMdd_HHmmss}.png",
